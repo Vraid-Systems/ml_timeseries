5318cceb965d42423883396cda844e4a1e99bd1d
Jason Zerbe <jzerbe@vraidsys.com>
2021-01-10 21:09:01 -0700

    environment flag to save to firestore or not

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ const Prediction = require('./Prediction')
 
 const cryptoTickers = process.env.CRYPTO ? process.env.CRYPTO.split(',').map((cryptoTicker) => `CRYPTO:${cryptoTicker}`) : []
 const model = process.env.MODEL ? process.env.MODEL.toLowerCase() : 'arima'
+const savePrediction = !!process.env.SAVE
 const stockTickers = process.env.STOCK ? process.env.STOCK.split(',').map((stockTicker) => `STOCK:${stockTicker}`) : []
 
 const main = async () => {
     const prediction = new Prediction(model, lodash.union(cryptoTickers, stockTickers))
-    await prediction.writeToFirestore()
+    if (savePrediction) {
+        await prediction.writeToFirestore()
+    }
     return prediction.writeToFile()
 }
 
